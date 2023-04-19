2c0fee316131046be0de0dee1ca7cf294eff69f5
Kaung Myat Thet Mon <kaungmyatthetmon@gmail.com>
2023-04-19 16:34:04 +0800

    UI Test for App Transparency Tracking

--- Experiments/ViewController.swift
+++ Experiments/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import AppTrackingTransparency
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+      ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+        switch status {
+        case .notDetermined:
+          print("Unknown consent")
+        case .restricted:
+          print("Device has an MDM solution applied")
+        case .denied:
+          print("Denied consent")
+        case .authorized:
+          print("Granted consent")
+        default:
+          print("Unknown")
+        }
+      })
+    }
   }
 
 
